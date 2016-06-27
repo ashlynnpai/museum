db0672dd124ed53e3447327be856237c2f157fb0
Ashlynn Pai <ashlynnpai@gmail.com>
2016-06-27 20:15:31 +0000

    add comments create

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   
-  resources :places#, defaults: {format: :json}
+  resources :places do
+    resources :comments, only: :create
+  end
 end
