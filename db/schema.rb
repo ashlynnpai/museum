753cad72fe1685e8f7e4af413b2ce60d56a67f6c
Ashlynn Pai <ashlynnpai@gmail.com>
2016-06-28 15:49:28 +0000

    enable carrierwave for photo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627212956) do
+ActiveRecord::Schema.define(version: 20160628144839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160627212956) do
     t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
